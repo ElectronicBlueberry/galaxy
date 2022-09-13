2cf06de1ed19a937607367390a48c1224b361fb8
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-09-13 15:55:56 +0200

    add keyboard toggleable modal

---
+++ client/src/components/CommandPalette.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import { useKeyUp } from "composables/events";
+
+const modal = ref(null);
+const { onKeyUp } = useKeyUp();
+
+onKeyUp("P", { shiftKey: true }, () => {
+    modal.value.toggle();
+});
+</script>
+
+<template>
+    <b-modal ref="modal" hide-header hide-footer> Hello World </b-modal>
+</template>
---
+++ client/src/composables/events.js
@@ -0,0 +1,48 @@
+import { onMounted, onBeforeUnmount } from "vue";
+
+function matchKeyEvent(key, modifiers, event) {
+    if (key === event.key) {
+        // check if all modifiers match the event
+        for (const [key, value] of Object.entries(modifiers ?? {})) {
+            if (event[key] !== value) {
+                return false;
+            }
+        }
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export function useKeyUp() {
+    const callbacks = [];
+
+    const onKeyUp = (key, modifiers, callbackFunction) => {
+        const callback = {
+            key,
+            modifiers,
+            callbackFunction,
+        };
+
+        callbacks.push(callback);
+    };
+
+    const eventHandler = (e) => {
+        callbacks.forEach((callback) => {
+            if (matchKeyEvent(callback.key, callback.modifiers, e)) {
+                callback.callbackFunction();
+            }
+        });
+    };
+
+    onMounted(() => {
+        document.addEventListener("keyup", eventHandler);
+    });
+
+    onBeforeUnmount(() => {
+        document.removeEventListener("keyup", eventHandler);
+    });
+
+    return { onKeyUp };
+}
