4f5fab26d73d9744ecf2ebc63e2a30d1e299d4da
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-02-28 17:51:06 +0100

    add base scrolling logic

--- client/src/components/VirtualScroller/VirtualScroller.vue
+++ client/src/components/VirtualScroller/VirtualScroller.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import VirtualScrollBar from "./VirtualScrollBar.vue";
-import { ref, reactive, type Ref, watch, computed } from "vue";
+import { ref, type Ref, watch, computed, type ComputedRef } from "vue";
+import { useAnimationFrame } from "@/composables/sensors/animationFrame";
+import { useAnimationFrameResizeObserver } from "@/composables/sensors/animationFrameResizeObserver";
 
 const props = defineProps<{
     items: Array<unknown>;
@@ -11,30 +13,62 @@ const emit = defineEmits<{
     (e: "windowScroll", fromIndex: number, toIndex: number): void;
 }>();
 
-const scrollWindow = reactive({
-    from: 0,
-    to: 0,
+const virtualScrollBar: Ref<InstanceType<typeof VirtualScrollBar> | null> = ref(null);
+const virtualScrollBarElement: ComputedRef<HTMLElement | null> = computed(() => {
+    const el = virtualScrollBar.value?.$el;
+    if (el && typeof el !== "string") {
+        return el as HTMLElement;
+    } else {
+        return null;
+    }
+});
+const itemOffset = ref(0);
+const itemCount = ref(0);
+useAnimationFrameResizeObserver(virtualScrollBarElement, ({ clientSize }) => {
+    const height = clientSize.height;
+    itemCount.value = Math.ceil(height / props.estimatedItemHeight);
+});
+
+const scrollWindow = computed(() => {
+    return {
+        from: itemOffset.value,
+        to: itemOffset.value + itemCount.value,
+    };
 });
 
 watch(
-    () => scrollWindow,
+    () => scrollWindow.value,
     ({ from, to }) => {
         emit("windowScroll", from, to);
     }
 );
 
-const itemsInWindow = computed(() => props.items.slice(scrollWindow.from, scrollWindow.to));
+const itemsInWindow = computed(() => props.items.slice(scrollWindow.value.from, scrollWindow.value.to));
 
-const virtualScrollBarHeight = ref(0);
-const virtualScrollBar: Ref<typeof VirtualScrollBar | null> = ref(null);
+const contentWindow: Ref<Element | null> = ref(null);
+const virtualScrollBarHeight = computed(() =>
+    Math.max(itemsInWindow.value.length * props.estimatedItemHeight, contentWindow.value?.scrollHeight ?? 0)
+);
 
-const contentWindowPosition = ref(0);
+const contentWindowOffset = ref(0);
+
+//let relativeScroll = 0;
+function virtualScroll(relativePosition: number) {
+    //    relativeScroll = relativePosition;
+}
+
+useAnimationFrame(() => {
+    //const indexOffset = (relativeScroll * virtualScrollBarHeight.value) / props.estimatedItemHeight;
+});
 </script>
 
 <template>
-    <VirtualScrollBar ref="virtualScrollBar" :height="virtualScrollBarHeight">
-        <div class="virtual-scroller-content-window" :style="`--position-top:${contentWindowPosition}px`">
-            <div v-for="(item, i) in itemsInWindow" :key="i">
+    <VirtualScrollBar ref="virtualScrollBar" :height="virtualScrollBarHeight" @virtual-scroll="virtualScroll">
+        <div
+            ref="contentWindow"
+            class="virtual-scroller-content-window"
+            :style="`--position-top:${contentWindowOffset}px`">
+            <div v-for="(item, i) in itemsInWindow" :id="`virtual-scroll-bar-content-${i}`" :key="i">
                 <slot name="item" :item="item" :window="scrollWindow"></slot>
             </div>
         </div>
