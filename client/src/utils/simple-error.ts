2dccb9fafda9147046a25b9335d5a6a00856db7c
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-28 17:44:07 +0200

    fix broken rebase

--- client/src/utils/simple-error.ts
+++ client/src/utils/simple-error.ts
@@ -8,23 +8,23 @@ interface ResponseError {
     };
 }
 
-export function errorMessageAsString(e: ResponseError | string, defaultMessage = "Request failed.") {
+function isResponseError(e: unknown): e is ResponseError {
+    return typeof e === "object" && e !== null && "response" in e;
+}
+
+export function errorMessageAsString(e: ResponseError | string | Error, defaultMessage = "Request failed.") {
     let message = defaultMessage;
     if (typeof e == "string") {
         message = e;
-    } else if (e && e.response && e.response.data && e.response.data.err_msg) {
+    } else if (isResponseError(e) && e.response.data && e.response.data.err_msg) {
         message = e.response.data.err_msg;
-    } else if (e && e.response) {
+    } else if (isResponseError(e)) {
         message = `${e.response.statusText} (${e.response.status})`;
-    } else if (e instanceof Error) {
-        message = e.message;
-    } else if (typeof e == "string") {
-        message = e;
     }
     return message;
 }
 
-export function rethrowSimple(e: ResponseError | string): never {
+export function rethrowSimple(e: ResponseError | string | Error): never {
     console.debug(e);
     throw Error(errorMessageAsString(e));
 }
