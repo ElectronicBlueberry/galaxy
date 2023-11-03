6ab4a460e835c69ee834bd7780bf993bae04405d
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-11-03 14:39:24 +0100

    refactor WorkflowRunForm to typescript and composition add type assertion utilities

--- client/src/components/Workflow/Run/WorkflowRunForm.vue
+++ client/src/components/Workflow/Run/WorkflowRunForm.vue
@@ -1,7 +1,226 @@
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed, ref } from "vue";
+
+import type { validateInputs } from "@/components/Form/utilities";
+import { allowCachedJobs } from "@/components/Tool/utilities";
+import { useHistoryStore } from "@/stores/historyStore";
+import { useUserStore } from "@/stores/userStore";
+import { assertDefined, maybeGetPropertyChain } from "@/utils/assertions";
+
+import { getReplacements, type WorkflowRunModel } from "./model";
+import { invokeWorkflow } from "./services";
+
+import WorkflowRunDefaultStep from "./WorkflowRunDefaultStep.vue";
+import WorkflowRunInputStep from "./WorkflowRunInputStep.vue";
+import ButtonSpinner from "@/components/Common/ButtonSpinner.vue";
+import FormCard from "@/components/Form/FormCard.vue";
+import FormDisplay from "@/components/Form/FormDisplay.vue";
+import FormElement from "@/components/Form/FormElement.vue";
+
+const props = defineProps<{
+    model: WorkflowRunModel;
+}>();
+
+const emit = defineEmits<{
+    (e: "submissionSuccess", invocations: unknown): void;
+    (e: "submissionError", error: Error): void;
+}>();
+
+const { currentUser, currentPreferences } = storeToRefs(useUserStore());
+const { currentHistoryId } = storeToRefs(useHistoryStore());
+
+const showExecuting = ref(true);
+
+type Value = unknown;
+type StepData = Record<string, Value>;
+
+const stepData = ref<Record<string, StepData>>({});
+const inputs = ref<Record<string, unknown>>({});
+
+function onDefaultStepInputs(stepId: string, data: { input: unknown }) {
+    inputs.value[stepId] = data.input;
+}
+
+function onToolStepInputs(stepId: string, data: StepData) {
+    stepData.value[stepId] = data;
+}
+
+const parameterInputs = computed(() => Object.values(props.model.wpInputs) ?? []);
+const parameterInputsAvailable = computed(() => parameterInputs.value.length > 0);
+const parameterData = ref<WorkflowRunModel["wpInputs"]>({});
+
+function onParameterInputs(newData: typeof parameterData.value) {
+    parameterData.value = newData;
+}
+
+function getReplacementParameters(inputs: typeof parameterData.value) {
+    return getReplacements(inputs, stepData.value, parameterData.value);
+}
+
+const resourceInputs = computed(() => Object.values(props.model.workflowResourceParameters) ?? []);
+const resourceInputsAvailable = computed(() => resourceInputs.value.length > 0);
+const resourceData = ref({});
+
+function onResourceInputs(newData: typeof resourceData.value) {
+    resourceData.value = newData;
+}
+
+const reuseAllowed = computed(() => allowCachedJobs(currentPreferences.value));
+
+const stepScrollTo = ref<{
+    stepId: string;
+    error: [string, string];
+}>();
+
+function getValidationScrollTo(stepId: string) {
+    if (stepScrollTo.value?.stepId === stepId) {
+        return stepScrollTo.value.error;
+    } else {
+        return [];
+    }
+}
+
+const historyInputs = [
+    {
+        type: "conditional",
+        name: "new_history",
+        test_param: {
+            name: "check",
+            label: "Send results to a new history",
+            type: "boolean",
+            value: "false",
+            help: "",
+        },
+        cases: [
+            {
+                value: "true",
+                inputs: [
+                    {
+                        name: "name",
+                        label: "History name",
+                        type: "text",
+                        value: props.model.name,
+                    },
+                ],
+            },
+            {
+                value: "false",
+                inputs: [],
+            },
+        ],
+    },
+];
+const historyData = ref<Record<string, string>>({});
+
+function onHistoryInputs(newData: typeof historyData.value) {
+    historyData.value = newData;
+}
+
+type Validation = ReturnType<typeof validateInputs>;
+
+const stepValidations = ref<Record<string, Validation>>({});
+
+function onValidation(stepId: string, validation: Validation) {
+    stepValidations.value[stepId] = validation;
+}
+
+const useCachedJobs = ref(false);
+
+async function onExecute() {
+    for (const [stepId, stepValidation] of Object.entries(stepValidations.value)) {
+        if (stepValidation) {
+            stepScrollTo.value = {
+                stepId: stepId,
+                error: stepValidation.slice() as [string, string],
+            };
+
+            return;
+        }
+    }
+
+    const parameters: Record<string, StepData> = {};
+
+    Object.entries(stepData.value).forEach(([stepId, stepData]) => {
+        const stepDataFiltered: Record<string, Value> = {};
+
+        Object.entries(stepData).forEach(([inputName, inputValue]) => {
+            if (!props.model.isConnected(stepId, inputName)) {
+                stepDataFiltered[inputName] = inputValue;
+            }
+        });
+
+        parameters[stepId] = stepDataFiltered;
+    });
+
+    const jobDef = {
+        new_history_name: historyData.value["new_history|name"] ? historyData.value["new_history|name"] : null,
+        history_id: !historyData.value["new_history|name"] ? props.model.historyId : null,
+        resource_params: resourceData.value,
+        replacement_params: parameterData.value,
+        use_cached_job: useCachedJobs.value,
+        inputs: inputs.value,
+        parameters: parameters.value,
+        // Tool form will submit flat maps for each parameter
+        // (e.g. "repeat_0|cond|param": "foo" instead of nested
+        // data structures).
+        parameters_normalized: true,
+        // Tool form always wants a list of invocations back
+        // so that inputs can be batched.
+        batch: true,
+        // the user is already warned if tool versions are wrong,
+        // they can still choose to invoke the workflow anyway.
+        require_exact_tool_versions: false,
+    } as const;
+
+    console.debug("WorkflowRunForm::onExecute()", "Ready for submission.", jobDef);
+    showExecuting.value = true;
+
+    try {
+        const invocations = await invokeWorkflow(props.model.workflowId, jobDef);
+
+        console.debug("WorkflowRunForm::onExecute()", "Submission successful.", invocations);
+        showExecuting.value = false;
+        emit("submissionSuccess", invocations);
+    } catch (e) {
+        console.debug("WorkflowRunForm::onExecute()", "Submission failed.", e);
+        showExecuting.value = false;
+
+        const errorData = maybeGetPropertyChain<Record<string, string>>(e, ["response", "data", "err_data"]);
+
+        if (errorData) {
+            try {
+                const errorEntries = Object.entries(errorData);
+                const stepId = errorEntries[0]?.[0];
+                const error = Object.entries(errorEntries[0]?.[1] ?? {})[0];
+
+                assertDefined(stepId);
+                assertDefined(error);
+
+                stepScrollTo.value = {
+                    stepId,
+                    error,
+                };
+            } catch (errorFormatting) {
+                console.debug(
+                    errorFormatting,
+                    "WorkflowRunForm::onExecute()",
+                    "Invalid server error response format.",
+                    errorData
+                );
+                emit("submissionError", e as Error);
+            }
+        } else {
+            emit("submissionError", e as Error);
+        }
+    }
+}
+</script>
+
 <template>
     <div v-if="currentUser && currentHistoryId" class="workflow-expanded-form">
         <div class="h4 clearfix mb-3">
-            <b>Workflow: {{ model.name }}</b>
+            <b>Workflow: {{ props.model.name }}</b>
             <ButtonSpinner
                 id="run-workflow"
                 class="float-right"
@@ -9,35 +228,32 @@
                 :wait="showExecuting"
                 @onClick="onExecute" />
         </div>
-        <FormCard v-if="wpInputsAvailable" title="Workflow Parameters">
-            <template v-slot:body>
-                <FormDisplay :inputs="wpInputs" @onChange="onWpInputs" />
-            </template>
+
+        <FormCard v-if="parameterInputsAvailable" title="Workflow Parameters">
+            <FormDisplay :inputs="parameterInputs" @onChange="onParameterInputs" />
         </FormCard>
+
         <FormCard title="History Options">
-            <template v-slot:body>
-                <FormDisplay :inputs="historyInputs" @onChange="onHistoryInputs" />
-            </template>
+            <FormDisplay :inputs="historyInputs" @onChange="onHistoryInputs" />
         </FormCard>
-        <FormCard v-if="reuseAllowed(currentUser)" title="Job re-use Options">
-            <template v-slot:body>
-                <FormElement
-                    v-model="useCachedJobs"
-                    title="Attempt to re-use jobs with identical parameters?"
-                    help="This may skip executing jobs that you have already run."
-                    type="boolean" />
-            </template>
+
+        <FormCard v-if="reuseAllowed" title="Job re-use Options">
+            <FormElement
+                v-model="useCachedJobs"
+                title="Attempt to re-use jobs with identical parameters?"
+                help="This may skip executing jobs that you have already run."
+                type="boolean" />
         </FormCard>
+
         <FormCard v-if="resourceInputsAvailable" title="Workflow Resource Options">
-            <template v-slot:body>
-                <FormDisplay :inputs="resourceInputs" @onChange="onResourceInputs" />
-            </template>
+            <FormDisplay :inputs="resourceInputs" @onChange="onResourceInputs" />
         </FormCard>
+
         <div v-for="step in model.steps" :key="step.index">
             <WorkflowRunDefaultStep
                 v-if="step.step_type == 'tool' || step.step_type == 'subworkflow'"
                 :model="step"
-                :replace-params="getReplaceParams(step.inputs)"
+                :replace-params="getReplacementParameters(step.inputs)"
                 :validation-scroll-to="getValidationScrollTo(step.index)"
                 :history-id="currentHistoryId"
                 @onChange="onToolStepInputs"
@@ -51,205 +267,3 @@
         </div>
     </div>
 </template>
-
-<script>
-import ButtonSpinner from "components/Common/ButtonSpinner";
-import FormCard from "components/Form/FormCard";
-import FormDisplay from "components/Form/FormDisplay";
-import FormElement from "components/Form/FormElement";
-import { allowCachedJobs } from "components/Tool/utilities";
-import { mapState } from "pinia";
-
-import { useHistoryStore } from "@/stores/historyStore";
-import { useUserStore } from "@/stores/userStore";
-
-import { getReplacements } from "./model";
-import { invokeWorkflow } from "./services";
-import WorkflowRunDefaultStep from "./WorkflowRunDefaultStep";
-import WorkflowRunInputStep from "./WorkflowRunInputStep";
-
-export default {
-    components: {
-        ButtonSpinner,
-        FormDisplay,
-        FormCard,
-        FormElement,
-        WorkflowRunDefaultStep,
-        WorkflowRunInputStep,
-    },
-    props: {
-        model: {
-            type: Object,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            showExecuting: false,
-            stepData: {},
-            stepValidations: {},
-            stepScrollTo: {},
-            wpData: {},
-            inputs: {},
-            historyData: {},
-            useCachedJobs: false,
-            historyInputs: [
-                {
-                    type: "conditional",
-                    name: "new_history",
-                    test_param: {
-                        name: "check",
-                        label: "Send results to a new history",
-                        type: "boolean",
-                        value: "false",
-                        help: "",
-                    },
-                    cases: [
-                        {
-                            value: "true",
-                            inputs: [
-                                {
-                                    name: "name",
-                                    label: "History name",
-                                    type: "text",
-                                    value: this.model.name,
-                                },
-                            ],
-                        },
-                        {
-                            value: "false",
-                            inputs: [],
-                        },
-                    ],
-                },
-            ],
-        };
-    },
-    computed: {
-        ...mapState(useUserStore, ["currentUser"]),
-        ...mapState(useHistoryStore, ["currentHistoryId"]),
-        resourceInputsAvailable() {
-            return this.resourceInputs.length > 0;
-        },
-        resourceInputs() {
-            return this.toArray(this.model.workflowResourceParameters);
-        },
-        wpInputsAvailable() {
-            return this.wpInputs.length > 0;
-        },
-        wpInputs() {
-            return this.toArray(this.model.wpInputs);
-        },
-    },
-    methods: {
-        reuseAllowed(user) {
-            return allowCachedJobs(user.preferences);
-        },
-        getReplaceParams(inputs) {
-            return getReplacements(inputs, this.stepData, this.wpData);
-        },
-        getValidationScrollTo(stepId) {
-            if (this.stepScrollTo.stepId == stepId) {
-                return this.stepScrollTo.stepError;
-            }
-            return [];
-        },
-        onDefaultStepInputs(stepId, data) {
-            this.inputs[stepId] = data.input;
-        },
-        onToolStepInputs(stepId, data) {
-            this.stepData[stepId] = data;
-        },
-        onHistoryInputs(data) {
-            this.historyData = data;
-        },
-        onResourceInputs(data) {
-            this.resourceData = data;
-        },
-        onWpInputs(data) {
-            this.wpData = data;
-        },
-        onValidation(stepId, validation) {
-            this.stepValidations[stepId] = validation;
-        },
-        onExecute() {
-            for (const [stepId, stepValidation] of Object.entries(this.stepValidations)) {
-                if (stepValidation) {
-                    this.stepScrollTo = {
-                        stepId: stepId,
-                        stepError: stepValidation.slice(),
-                    };
-                    return;
-                }
-            }
-
-            const parameters = {};
-            Object.entries(this.stepData).forEach(([stepId, stepData]) => {
-                const stepDataFiltered = {};
-                Object.entries(stepData).forEach(([inputName, inputValue]) => {
-                    if (!this.model.isConnected(stepId, inputName)) {
-                        stepDataFiltered[inputName] = inputValue;
-                    }
-                });
-                parameters[stepId] = stepDataFiltered;
-            });
-
-            const jobDef = {
-                new_history_name: this.historyData["new_history|name"] ? this.historyData["new_history|name"] : null,
-                history_id: !this.historyData["new_history|name"] ? this.model.historyId : null,
-                resource_params: this.resourceData,
-                replacement_params: this.wpData,
-                use_cached_job: this.useCachedJobs,
-                inputs: this.inputs,
-                parameters: parameters,
-                // Tool form will submit flat maps for each parameter
-                // (e.g. "repeat_0|cond|param": "foo" instead of nested
-                // data structures).
-                parameters_normalized: true,
-                // Tool form always wants a list of invocations back
-                // so that inputs can be batched.
-                batch: true,
-                // the user is already warned if tool versions are wrong,
-                // they can still choose to invoke the workflow anyway.
-                require_exact_tool_versions: false,
-            };
-
-            console.debug("WorkflowRunForm::onExecute()", "Ready for submission.", jobDef);
-            this.showExecuting = true;
-            invokeWorkflow(this.model.workflowId, jobDef)
-                .then((invocations) => {
-                    console.debug("WorkflowRunForm::onExecute()", "Submission successful.", invocations);
-                    this.showExecuting = false;
-                    this.$emit("submissionSuccess", invocations);
-                })
-                .catch((e) => {
-                    console.debug("WorkflowRunForm::onExecute()", "Submission failed.", e);
-                    this.showExecuting = false;
-                    const errorData = e && e.response && e.response.data && e.response.data.err_data;
-                    if (errorData) {
-                        try {
-                            const errorEntries = Object.entries(errorData);
-                            this.stepScrollTo = {
-                                stepId: errorEntries[0][0],
-                                stepError: Object.entries(errorEntries[0][1])[0],
-                            };
-                        } catch (errorFormatting) {
-                            console.debug(
-                                errorFormatting,
-                                "WorkflowRunForm::onExecute()",
-                                "Invalid server error response format.",
-                                errorData
-                            );
-                            this.$emit("submissionError", e);
-                        }
-                    } else {
-                        this.$emit("submissionError", e);
-                    }
-                });
-        },
-        toArray(obj) {
-            return obj ? Object.keys(obj).map((k) => obj[k]) : [];
-        },
-    },
-};
-</script>
--- client/src/utils/assertions.ts
+++ client/src/utils/assertions.ts
@@ -1,3 +1,10 @@
+/*
+
+    This module contains runtime typechecking and assertion utilities,
+    to help in quickly narrowing down a type in a typesafe manner.
+
+*/
+
 /**
  * Asserts that a value is not undefined or null
  * @param value value to test
@@ -30,4 +37,80 @@ export function assertArray(value: unknown, error?: string | Error): asserts val
         const message = error ?? TypeError(`Value is not an Array`);
         throw message instanceof Error ? message : TypeError(message);
     }
+}
+
+type Property = string | number | symbol;
+
+/**
+ * Returns whether or not a value has a specific property
+ *
+ * @param value Potentially an object, which may have `property`
+ * @param property property to search for
+ * @returns `true` if property is in value
+ */
+export function hasProperty<T>(value: T, property: Property): boolean {
+    return typeof value === "object" && value !== null && property in value;
+}
+
+/**
+ * Assert version of the `hasProperty` utility
+ *
+ * @param value Potentially an object, which may have `property`
+ * @param property property to search for
+ * @param error optional error message, or Error object
+ *
+ * @see hasProperty
+ */
+export function assertProperty<T>(
+    value: T,
+    property: Property,
+    error?: string | Error
+): asserts value is T & Record<typeof property, unknown> {
+    if (!hasProperty(value, property)) {
+        const propertyString = typeof property === "symbol" ? "Symbol" : `${property}`;
+        const message = error ?? TypeError(`Property "${propertyString}" does not exist on value "${value}"`);
+        throw message instanceof Error ? message : TypeError(message);
+    }
+}
+
+/**
+ * Uses the `assertProperty` assertion to return the value of a property only if it exists.
+ *
+ * Use the type parameter to type the return appropriately.
+ *
+ * @param value Potentially an object, which may have `property`
+ * @param property property to search for
+ * @returns Value of property or `undefined`
+ *
+ * @see assertProperty
+ */
+export function maybeGetProperty<R = unknown>(value: unknown, property: Property): R | undefined {
+    try {
+        assertProperty(value, property);
+        return value[property] as R;
+    } catch (e) {
+        return undefined;
+    }
+}
+
+/**
+ * Uses the `maybeGetProperty` utility to return the value a the end of a chain of properties,
+ * only if all properties and child properties exist.
+ *
+ * Use the type parameter to type the return appropriately.
+ *
+ * @param value Potentially an object
+ * @param property property chain to search
+ * @returns Value of property and end of chain or `undefined`
+ *
+ * @see maybeGetProperty
+ */
+export function maybeGetPropertyChain<R = unknown>(value: unknown, propertyChain: Property[]): R | undefined {
+    let nextValue: unknown = value;
+
+    propertyChain.forEach((property) => {
+        nextValue = maybeGetProperty(nextValue, property);
+    });
+
+    return nextValue as R | undefined;
 }
